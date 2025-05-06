409ec0423e7be7cdcf70cd3f59f98d019a731546
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-06 11:50:20 +0700

    membuat class lingkaran

--- File1.cpp
+++ File1.cpp
@@ -24,3 +24,19 @@ class PersegiPanjang
     Lingkaran O1;
 
 };
+
+class Lingkaran
+{
+    public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin  >> jarijari;
+    }
+    int luaslingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
