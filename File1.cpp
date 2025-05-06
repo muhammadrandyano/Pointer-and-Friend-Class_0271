a3837f34b4cf777f6bf6befaeca911e6b2bd8603
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-05-06 11:49:24 +0700

    membuat class persegipanjang

--- File1.cpp
+++ File1.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class PersegiPanjang
+{
+    public:
+    int panjang, lebar;
+
+    public:
+
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran O1;
+
+};
